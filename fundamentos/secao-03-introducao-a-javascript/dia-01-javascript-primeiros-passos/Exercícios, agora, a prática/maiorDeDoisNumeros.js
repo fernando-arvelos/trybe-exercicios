b6bd9a49d509c9079a4e4a65dd3d0f53909ec6b7
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-02-28 09:22:35 -0300

    Maior de dois números

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/Exercícios, agora, a prática/maiorDeDoisNumeros.js
@@ -0,0 +1,10 @@
+let num1 = 31;
+let num2 = 31;
+
+if (num1 > num2) {
+  console.log('num1 é o maior número!');
+} else if (num2 > num1) {
+  console.log('num2 é o maior número!');
+} else {
+  console.log('Os números são iguais!');
+}
