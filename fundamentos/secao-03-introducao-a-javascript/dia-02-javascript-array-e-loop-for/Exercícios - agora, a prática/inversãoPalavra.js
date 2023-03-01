2cf22b4b53b8a9ac744dc6c36d94115835e8bfda
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-03-01 10:49:55 -0300

    Maior e menor palavra

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/Exercícios - agora, a prática/inversãoPalavra.js
@@ -0,0 +1,8 @@
+let word = 'Socorram me subi no onibus em marrocos';
+let reverseWord = '';
+
+for (let index = 0; index < word.length; index += 1) {
+  reverseWord += word[word.length - 1 - index];
+}
+
+console.log(reverseWord);
