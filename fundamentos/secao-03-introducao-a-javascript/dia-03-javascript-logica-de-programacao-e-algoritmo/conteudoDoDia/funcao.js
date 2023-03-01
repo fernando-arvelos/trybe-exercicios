051c7954b06a7f494455b27856d51327efd9abc9
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-03-01 15:41:57 -0300

    Funções

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmo/conteudoDoDia/funcao.js
@@ -0,0 +1,25 @@
+let saldo = 10;
+
+function adicionaSaldo (valor) {
+  return (saldo + valor)
+}
+
+console.log(adicionaSaldo(25))
+
+function subtraiSaldo (valor) {
+  return (saldo - valor)
+}
+
+console.log(subtraiSaldo(5));
+
+function multiplicaSaldo (valor) {
+  return (saldo * valor)
+}
+
+console.log(multiplicaSaldo(8));
+
+function divideSaldo (valor) {
+  return (saldo / valor)
+}
+
+console.log(divideSaldo(2));
