90aff4f459a4e28ab570802264c1cf4eb2c39078
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-03-01 10:39:01 -0300

    Fatorial de 10

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/Exercícios - agora, a prática/arrayDeUmAVinteCinco.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/Exercícios - agora, a prática/arrayDeUmAVinteCinco.js
@@ -1,7 +1,15 @@
-let numbers = []
+let numbers = [];
+let result = []
 
 for (let index = 0; index < 25; index += 1) {
   numbers.push(index + 1)
 }
 
 console.log(numbers);
+
+
+for (let index = 0; index < numbers.length; index += 1) {
+  result.push(numbers[index] / 2)
+}
+
+console.log(result);
